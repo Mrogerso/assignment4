a45376ff9ce99340b41db790b9d5aa809f1d1194
Mrogerso <130171489+Mrogerso@users.noreply.github.com>
2023-04-07 15:16:20 -0400

    Add files via upload
    Added a file

---
+++ hello.py
@@ -0,0 +1,4 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:38:17) [MSC v.1932 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# just print hello world
+print("Hello World!")
